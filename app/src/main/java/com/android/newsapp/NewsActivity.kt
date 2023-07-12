66d0f5b63510ec2da43f98668ab43885ab1157f6
paramvir <paramvir@192-168-1-120.tpgi.com.au>
2023-07-12 22:58:27 +0800

    added navigation tabs

---
+++ app/src/main/java/com/android/newsapp/NewsActivity.kt
@@ -0,0 +1,20 @@
+package com.android.newsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.news_layout)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigation)
+        navView.setupWithNavController(navController)
+    }
+
+}
